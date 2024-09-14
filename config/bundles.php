8141e6f7c169cb4b7f7248228d083a06cb6c5f11
Reuniko <reuniko@gmail.com>
2024-09-14 11:52:40 +0300

    auth and register

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,4 @@
-ss<?php
+<?php
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -7,30 +7,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
 
-    #[Route('/register', name: 'register')]
+    #[Route('/register', name: 'app_register_form', methods: ['GET'])]
+    public function registerForm(
+        // ...
+    ): Response
+    {
+        return $this->render('registration/register.html.twig');
+    }
+
+
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
     public function register(
         Request                     $request,
         EntityManagerInterface      $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
+        UserPasswordHasherInterface $passwordHasher
     ): Response
     {
-        $user = new \App\Entity\User();
-        $plaintextPassword = $request->request->get('password');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $plaintextPassword
-        );
-        $user->setPassword($hashedPassword);
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        // Валидация данных
+        if (empty($email) || empty($password)) {
+            // Обработайте ошибку валидации
+            return $this->render('registration/register.html.twig', [
+                'error' => 'Пожалуйста, введите email и пароль.',
+            ]);
+        }
+
+        $user = new \App\Entity\User();
+        $user->setEmail($email);
+        $user->setPassword($passwordHasher->hashPassword($user, $password));
 
         $entityManager->persist($user);
         $entityManager->flush();
 
-        // ...
+        return $this->redirectToRoute('app_login');
     }
 }
