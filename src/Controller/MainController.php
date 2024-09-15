2159d534e6299b87e0597ece29f90e90bcf73544
Reuniko <reuniko@gmail.com>
2024-09-15 10:13:55 +0300

    добавление товаров в персональную корзину пользователя

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Cart;
+use App\Entity\CartProduct;
+use App\Entity\Product;
+use App\Repository\CartProductRepository;
 use App\Repository\CartRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -31,40 +34,71 @@ class MainController extends AbstractController
         ProductRepository      $productRepository,
         CartRepository         $cartRepository,
         EntityManagerInterface $entityManager,
+        CartProductRepository  $cartProductRepository,
     ): Response
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirectToRoute('app_login');
         }
 
+        $message = '';
+
         // Обработка добавления в корзину (без AJAX)
         if ($request->isMethod('POST')) {
             $productId = $request->request->get('product_id');
+            /**@var Product $product */
             $product = $productRepository->find($productId);
 
             if ($product) {
                 // Логика добавления товара в корзину
-                $user_cart = $cartRepository->findBy([
+                $cart = $cartRepository->findOneBy([
                     'isPay' => 0,
                     'userId' => $this->getUser()->getId(),
                 ]);
-                //print_r($user_cart);
-                //print_r($this->getUser()->getId());
-                if (empty($user_cart)) {
-                    $user_cart = new Cart();
-                    $user_cart->setPay(false);
-                    $user_cart->setUserId($this->getUser()->getId());
-                    $entityManager->persist($user_cart);
+                //$this->debug($user_cart);
+                //$this->debug($this->getUser()->getId());
+                if (empty($cart)) {
+                    $cart = new Cart();
+                    $cart->setPay(false);
+                    $cart->setUserId($this->getUser()->getId());
+                    $entityManager->persist($cart);
                     $entityManager->flush();
                 }
-                //print_r($user_cart);
+                //$this->debug($user_cart);
+                //$this->debug($product);
 
+                /**@var CartProduct $cartProduct */
+                $cartProduct = $cartProductRepository->findOneBy([
+                    'cartId' => $cart->getId(),
+                    'productId' => $product->getId(),
+                ]);
+
+                if (!empty($cartProduct)) {
+                    $cartProduct->setAmount($cartProduct->getAmount() + 1);
+                } else {
+                    $cartProduct = new CartProduct();
+                    $cartProduct->setCartId($cart->getId());
+                    $cartProduct->setProductId($product->getId());
+                    $cartProduct->setAmount(1);
+                }
+                $entityManager->persist($cartProduct);
+                $entityManager->flush();
+
+                $message = "Товар '{$product->getName()}' добавлен в корзину в количестве - {$cartProduct->getAmount()}";
+
+                //$this->debug($cartProduct);
             }
         }
 
         $products = $productRepository->findAll();
         return $this->render('main\\index.html.twig', [
             'products' => $products,
+            'message' => $message,
         ]);
+    }
+
+    public function debug($info)
+    {
+        echo "<xmp>" . print_r($info, true) . "</xmp>";
     }
 }
